5986079d27d2c135f209680bf4fadb7faefbeaf5
Abhaykumarrai <abhaykumarrai666@gmail.com>
2024-10-12 13:54:23 +0530

    update 6

---
+++ typeof.js
@@ -0,0 +1,18 @@
+// You can use the JavaScript typeof operator to find the type of a JavaScript variable.
+// The typeof operator returns the type of a variable or an expression:
+
+let user = "Abhay"
+let x = 98
+let Boolean = true
+let cid 
+let man = null
+let y = BigInt('5777777777777777778378348358738')
+
+console.log(typeof"user")
+console.log(typeof x)
+console.log(typeof  Boolean)
+console.log(typeof cid)
+console.log(typeof man)
+console.log(typeof y)
+
+// CONCLUSION Typeof null data type is object
