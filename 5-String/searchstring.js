32d318e7377598def755914ed1ffaf6058d8a1b0
Abhaykumarrai <abhaykumarrai666@gmail.com>
2024-10-13 18:31:48 +0530

    update string

--- 5-String/searchstring.js
+++ 5-String/searchstring.js
@@ -1,3 +1,6 @@
+//all are case sensitive
+
+
 // indexOf() - The indexOf() method returns the index (position) of the first occurrence of a string in a string, or it returns -1 if the string is not found:
 let text = "Please locate where 'locate' occurs!";
 console.log(text.indexOf("occurs"))
@@ -12,10 +15,24 @@ console.log(text.search("occurs"))
 
 //Note - it return -1 in case of no match
 
-// String match()
+// String match() - The match() method returns an array containing the results of matching a string against a string (or a regular expression).
+let text5 = "The rain in SPAIN stays mainly in the plain";
+console.log(text5.match("nly"))
+
+
+
 // String matchAll()
-// String includes() 
-// String startsWith()
-// String endsWith()
 
 
+// String includes() - The includes() method returns true if a string contains a specified value.Otherwise it returns false.
+let exe = "my name is abhay"
+console.log(exe.includes("abhay",11)) // check name abhay at index no 11
+
+
+// String startsWith()- The startsWith() method returns true if a string begins with a specified value.Otherwise it returns false:
+// String endsWith() - The endsWith() method returns true if a string ends with a specified value.Otherwise it returns false:
+let exe1 = "my name is abhay"
+console.log(exe1.startsWith("my")) //gives true
+console.log(exe1.startsWith("name")) //Gives false
+console.log(exe1.endsWith("abhay")) //gives true
+console.log(exe1.endsWith("my")) //gives false
